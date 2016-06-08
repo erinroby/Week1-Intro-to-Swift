ced807f39f29b8be0a9b260a11f28992b8847fa5
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-06-08 15:42:39 -0700

    define Todo class, Identity protocol, and ObjectStoreProtocol. Outline Store class and add / removing of ToDo Items

--- class-3/ToDoHomework.playground/Contents.swift
+++ class-3/ToDoHomework.playground/Contents.swift
@@ -10,9 +10,15 @@ protocol Identity {
 //Declare a ToDo class that conforms to Identity protocol… Define your own model scheme (variable it contains, etc)
 class Todo: Identity {
     var id: String
+    let task: String
     
-    init() {
+    init(task: String) {
         self.id = NSUUID().UUIDString
+        self.task = task
+    }
+    
+    func description() -> String {
+        return "\(self.task)"
     }
 }
 
@@ -29,6 +35,26 @@ protocol ObjectStoreProtocol {
 
 //Extend ObjectStore protocol to provide basic implementation for functions
 
+
+
 //Create Store singleton that will conform to ObjectStore protocol and implement requirements
 
+//class Store: ObjectStoreProtocol {
+//    static let shared = Store()
+//}
+
 //Demonstrate adding / removing of ToDo items.
+
+//let taskOne = Todo("Buy Beer")
+//let taskTwo = Todo("Drink Beer")
+//let taskThree = Todo("Nap")
+//
+//Store.shared.add(taskOne)
+//Store.shared.add(taskTwo)
+//Store.shared.add(taskThree)
+//
+//Store.shared.remove(taskTwo)
+//
+//for object in Store.shared.allObjects() {
+//    print(object.description)
+//}
